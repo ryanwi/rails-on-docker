8b246ce6202a4d25406452a8b098d509158ff6a4
Ryan W <rwilliamz@gmail.com>
2019-08-30 21:43:21 -0700

    Webcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   resources :foos
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  root 'welcome#index'
 end
