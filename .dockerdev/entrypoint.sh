1def410eab342fa55344fb4a187d1f59b184e9e6
Ryan W <rwilliamz@gmail.com>
2021-05-05 09:44:13 -0700

    yarn upgrade

--- .dockerdev/entrypoint.sh
+++ .dockerdev/entrypoint.sh
@@ -5,10 +5,10 @@ set -e
 rm -f /usr/src/app/tmp/pids/server.pid
 
 echo "bundle install..."
-# bundle check || bundle install --jobs 4
+bundle check || bundle install --jobs 4
 
 echo "yarn install..."
-# yarn install
+yarn install
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
