5921d2eaca4d953e114f3814cb2486aace614f6b
Ryan W <rwilliamz@gmail.com>
2022-01-19 11:47:46 -0800

    building with webpack

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,8 @@
 // Entry point for the build script in your package.json
+
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+// require("channels")
+
+console.log('Hello World from jsbundling-rails with webpack');
