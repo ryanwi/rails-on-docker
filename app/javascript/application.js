4405a3aecd012d491ac16e967b677650b1db5d1c
Ryan W <rwilliamz@gmail.com>
2022-01-19 11:55:01 -0800

    Migrate JS entrypoint

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,7 +3,7 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
-// import "channels"
+import "./channels"
 
 Rails.start()
 Turbolinks.start()
