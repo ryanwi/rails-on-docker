03ade7ab5fa3c408cc1c35485076e2a958b3e908
Ryan W <rwilliamz@gmail.com>
2022-01-19 11:51:32 -0800

    Migrating to webpack without webpacker

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,12 @@
 // Entry point for the build script in your package.json
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-// require("channels")
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+// import "channels"
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
 
 console.log('Hello World from jsbundling-rails with webpack');
