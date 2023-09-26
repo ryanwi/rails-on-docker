34cc63df03b7d2292863429039b70e63c3cf6440
Ryan W <rwilliamz@gmail.com>
2023-09-25 18:33:57 -0700

    Fix deprecation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
   config.consider_all_requests_local = true
 
   # Allow access when running in docker
-  config.web_console.whitelisted_ips = ["172.16.0.0/12", "192.168.0.0/16"]
+  config.web_console.allowed_ips = ["172.16.0.0/12", "192.168.0.0/16"]
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
